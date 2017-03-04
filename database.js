4ba9350ea96c38971d60999eafa7d233b7192b51
Hanna Bennett <bennetthanna22@gmail.com>
2017-03-04 10:11:52 -0800

    Finds users

--- database.js
+++ database.js
@@ -1,14 +1,29 @@
 var mongo = require('mongodb');
 var server = new mongo.Server('localhost',27017,{auto_reconnect:true});
-var database = new mongo.Db('users',server);
+var database = new mongo.Db('rps',server);
+
+database.open(function(err,db) {    
+    if(!err) {        
+        console.log("Connected to 'rps' database");
+        db.collection('users',{strict:true},function(err,collection){
+            if(err) { 
+                console.log("The users collection doesn't exist");
+            }
+        })
+    } else {
+        console.log("Looks like a db error."+ err);
+    }
+})
 
 exports.findUsers = function(req,res) {
     console.log("in findUsers");
     var cursor = database.collection('users').find( ).toArray(function (err, result) {
         if(!err) {
              for(var i = 0;i < result.length; ++i) {
-	            res.write(result[i].username+ " " +result[i].cwc+"<br/>");             
+	            res.write(result[i].username+ " " +result[i].cwc);             
 	        } 
+        } else {
+            console.log(err);
         }
         res.end();
     });
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.get('/findUsers', database.findUsers);
+
 var server = app.listen(8080, function() {
 	console.log("server listening at 8080");
 });
